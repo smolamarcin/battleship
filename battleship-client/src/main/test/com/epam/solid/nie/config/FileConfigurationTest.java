4060e5044364704b786c5931ea1ea8efc15a0332
rivalez <marektabor1@gmail.com>
2017-12-06 14:15:32 +0100

    configuration file

---
+++ battleship-client/src/main/test/com/epam/solid/nie/config/FileConfigurationTest.java
@@ -0,0 +1,22 @@
+package com.epam.solid.nie.config;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+@Test
+public class FileConfigurationTest {
+
+    public void shouldProvideConfig(){
+        //given
+        Configuration conf = new FileConfiguration();
+
+        //when
+        Map<ConfigProperty, String> config = conf.provide();
+
+        //then
+        Assert.assertEquals(config.size(), 10);
+        Assert.assertEquals(config.get(ConfigProperty.BOARD_SIZE), "10");
+    }
+}
