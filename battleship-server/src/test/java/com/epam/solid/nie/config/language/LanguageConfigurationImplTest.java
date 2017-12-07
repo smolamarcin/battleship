26828d75fc428e96f8ee732c2d19d87dbe46c122
Marcin <marcin@mbp-macbook.home>
2017-12-07 23:44:14 +0100

    deleted unnecessary tests, deleted throwing exceptions in test signatures

--- battleship-server/src/test/java/com/epam/solid/nie/config/language/LanguageConfigurationImplTest.java
+++ battleship-server/src/test/java/com/epam/solid/nie/config/language/LanguageConfigurationImplTest.java
@@ -6,14 +6,15 @@ import static org.testng.Assert.*;
 
 public class LanguageConfigurationImplTest {
     private LanguageConfiguration languageConfiguration = new LanguageConfigurationImpl();
+
     @Test
-    public void shouldRead_Default_LanguageValue_FromFile() throws Exception {
+    public void shouldRead_Default_LanguageValue_FromFile() {
         Language currentLanguage = languageConfiguration.provide();
         assertTrue(currentLanguage.equals(Language.ENGLISH));
     }
 
     @Test
-    public void shouldLoad_AnyLanguage_FromFile() throws Exception {
+    public void shouldLoad_AnyLanguage_FromFile() {
         assertNotNull(languageConfiguration.provide());
     }
 }
