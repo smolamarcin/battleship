372077f37e24855725b1a8036d5fda3ad132821f
marcin <marcin.smola7@gmail.com>
2017-12-07 13:15:55 +0100

    refactored test to language configuration

--- battleship-server/src/main/test/java/com/com/epam/solid/nie/config/language/CommunicateProviderImplTest.java
+++ battleship-server/src/main/test/java/com/com/epam/solid/nie/config/language/CommunicateProviderImplTest.java
@@ -23,25 +23,23 @@ public class CommunicateProviderImplTest {
 
     @DataProvider
     public static Object[][] communicatesInEnglish() {
-        return new Object[][]{{"Welcome in my game.", Communicate.WELCOME},
+        return new Object[][]{{Language.ENGLISH,"Welcome in my game.", Communicate.WELCOME},
         };
     }
 
     @Test (dataProvider = "communicatesInEnglish")
-    public void shouldLoadCorrectCommunicates_English(String expectedCommunicate,Communicate communicate){
-        Language language = Language.ENGLISH;
+    public void shouldLoadCorrectCommunicates_English(Language language,String expectedCommunicate,Communicate communicate){
         communicateProvider.populate(language);
         assertEquals(expectedCommunicate,communicateProvider.getCommunicate(communicate));
     }
     @DataProvider
     public static Object[][] communicatesInPolish() {
-        return new Object[][]{{"Witaj w  mojej grze.", Communicate.WELCOME},
+        return new Object[][]{{Language.POLISH,"Witaj w  mojej grze.", Communicate.WELCOME},
         };
     }
 
     @Test (dataProvider = "communicatesInPolish")
-    public void shouldLoadCorrectCommunicates_Polish(String expectedCommunicate,Communicate communicate){
-        Language language = Language.POLISH;
+    public void shouldLoadCorrectCommunicates_Polish(Language language,String expectedCommunicate,Communicate communicate){
         communicateProvider.populate(language);
         assertEquals(expectedCommunicate,communicateProvider.getCommunicate(communicate));
     }
