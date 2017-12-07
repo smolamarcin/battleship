b452bcf23eb7df515d7f29425bbe71fdb78a28fe
Dominik <saidamyzo@gmail.com>
2017-12-07 16:37:12 +0100

    Do different machines communication test

--- battleship-server/src/main/java/com.epam.solid.nie/server/CommunicationProtocol.java
+++ battleship-server/src/main/java/com.epam.solid.nie/server/CommunicationProtocol.java
@@ -2,7 +2,7 @@ package com.epam.solid.nie.server;
 
 public class CommunicationProtocol implements Protocol {
     @Override
-    public String processInput(String inputString) {
+    public String processInput(String inputString, int id) {
         return inputString.equals("Q") ? "Bye." : "Provide location";
     }
 }
