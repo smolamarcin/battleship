a99da9dc48b4f2f60eccca1d81cb6dcfdb65f2e3
marcin <marcin.smola7@gmail.com>
2017-12-07 09:19:57 +0100

    provide communicates basic implementation

---
+++ battleship-server/src/main/java/com/epam/solid/nie/config/language/CommunicateProviderImpl.java
@@ -0,0 +1,29 @@
+package com.epam.solid.nie.config.language;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import static com.epam.solid.nie.config.language.Communicate.WELCOME;
+
+public class CommunicateProviderImpl implements CommunicateProvider {
+    private Map<Communicate, String> communicates = new HashMap<>();
+
+    public CommunicateProviderImpl populate(Language language) {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle(language.name());
+        communicates.put(WELCOME, resourceBundle.getString(WELCOME.name()));
+        Stream.of(Communicate.values()).forEach(consume(resourceBundle));
+        return this;
+    }
+
+    private Consumer<Communicate> consume(ResourceBundle resourceBundle) {
+        return element -> communicates.put(element, resourceBundle.getString(element
+                .name()));
+    }
+
+    public String getCommunicate(Communicate communicate) {
+        return communicates.get(communicate);
+    }
+}
