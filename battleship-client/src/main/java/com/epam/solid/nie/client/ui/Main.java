9d9db2ca605cf9c602d33847ad5e45e03c7f282b
marcin <marcin.smola7@gmail.com>
2017-12-08 12:01:44 +0100

    basic layout

--- battleship-client/src/main/java/com/epam/solid/nie/client/ui/Main.java
+++ battleship-client/src/main/java/com/epam/solid/nie/client/ui/Main.java
@@ -10,8 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello world");
-        primaryStage.setScene(new Scene(root,300,275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
     public static void main(String[] args) {
