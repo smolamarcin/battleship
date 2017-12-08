c318caf80d325d94d334454465d4af1a00006b89
Ozymadias <saidamyzo@gmail.com>
2017-12-08 09:54:16 +0100

    Update CommunicationProtocol.java

--- battleship-server/src/main/java/com.epam.solid.nie/server/CommunicationProtocol.java
+++ battleship-server/src/main/java/com.epam.solid.nie/server/CommunicationProtocol.java
@@ -2,7 +2,7 @@ package com.epam.solid.nie.server;
 
 public class CommunicationProtocol implements Protocol {
     @Override
-    public String processInput(String inputString, int id) {
+    public String processInput(String inputString) {
         return inputString.equals("Q") ? "Bye." : "Provide location";
     }
 }
