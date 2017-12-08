c025d407d17a2a181b770e6610a89e9c4bf4807c
rivalez <marektabor1@gmail.com>
2017-12-08 09:41:32 +0100

    code review fix

--- battleship-client/src/main/java/com/epam/solid/nie/client/Main.java
+++ battleship-client/src/main/java/com/epam/solid/nie/client/Main.java
@@ -2,7 +2,7 @@ package com.epam.solid.nie.client;
 
 public class Main {
     public static void main(String[] args) {
-        ShipClient shipServer = new ShipSocketClient();
-        shipServer.run();
+        ShipClient shipClient = new ShipSocketClient();
+        shipClient.run();
     }
 }
