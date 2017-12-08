285fac3f033193dee1976ddeef9d3981a1855ab5
Ozymadias <saidamyzo@gmail.com>
2017-12-08 09:53:41 +0100

    Update Main.java

--- battleship-client/src/main/java/com/epam/solid/nie/client/Main.java
+++ battleship-client/src/main/java/com/epam/solid/nie/client/Main.java
@@ -3,7 +3,7 @@ package com.epam.solid.nie.client;
 public class Main {
     public static void main(String[] args) {
         System.out.println("server ip is: " + (args.length > 0 ? args[0] : ""));
-        ShipClient shipServer = new ShipSocketClient(args[0]);
-        shipServer.run();
+        ShipClient shipClient = new ShipSocketClient(args[0]);
+        shipClient.run();
     }
 }
