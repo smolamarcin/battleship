109a525aa0676240b0f5f3573ecb57769f4bcd0e
Marcin <marcin@MBP-Macbook.budapest.epam.com>
2017-12-18 11:57:07 +0100

    part1, deleted independent files

--- battleship-server/src/main/java/com.epam.solid.nie/server/server/CommunicationProtocol.java
+++
@@ -1,8 +0,0 @@
-package com.epam.solid.nie.server.server;
-
-public class CommunicationProtocol implements Protocol {
-    @Override
-    public String processInput(String inputString) {
-        return inputString.equals("Q") ? "Bye." : "Provide location";
-    }
-}
--- battleship-server/src/main/java/com.epam.solid.nie/server/server/Protocol.java
+++
@@ -1,5 +0,0 @@
-package com.epam.solid.nie.server.server;
-
-interface Protocol {
-    String processInput(String inputString);
-}
