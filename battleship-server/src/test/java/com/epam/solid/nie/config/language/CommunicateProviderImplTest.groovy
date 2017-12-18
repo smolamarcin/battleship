14b0bf0fb2f6d9e6e2aae83916e5625695e757dc
Marcin <marcin@MBP-Macbook.budapest.epam.com>
2017-12-18 13:07:29 +0100

    moved to valid directory

---
+++ battleship-server/src/test/java/com/epam/solid/nie/config/language/CommunicateProviderImplTest.groovy
@@ -0,0 +1,26 @@
+package com.epam.solid.nie.config.language;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CommunicateProviderImplTest {
+    private CommunicateProviderImpl communicateProvider = new CommunicateProviderImpl();
+
+    @DataProvider
+    public static Object[][] languages() {
+        return new Object[][]{{Language.ENGLISH},
+            {Language.POLISH}
+        }
+    }
+
+    @Test(dataProvider = "languages")
+    public void populateShould_FillCommunicatesMap(Language language) {
+        communicateProvider.populate(language);
+        assertFalse(communicateProvider.isMapEmpty());
+    }
+
+
+
+}
