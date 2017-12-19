1a7e6e2676ee3001d776122833c708372d5cbd1b
rivalez <marektabor1@gmail.com>
2017-12-19 14:27:43 +0100

    removed unused classes + continue removed

--- battleship-server/src/main/java/com/epam/solid/nie/server/Main.java
+++
@@ -1,8 +0,0 @@
-package com.epam.solid.nie.server;
-
-public class Main {
-    public static void main(String[] args) {
-        ShipServer shipServer = new ShipSocketServer(args.length > 0 ? args[0] : "");
-        shipServer.run();
-    }
-}
