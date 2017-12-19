c375f57e615cf7127db5001e29541d73624be955
rivalez <marektabor1@gmail.com>
2017-12-19 13:23:22 +0100

    removed log4j warnings

--- common/src/main/java/com/epam/solid/nie/state/EndState.java
+++ common/src/main/java/com/epam/solid/nie/state/EndState.java
@@ -1,17 +1,14 @@
 package com.epam.solid.nie.state;
 
 import lombok.ToString;
-import org.apache.log4j.Logger;
 
 @ToString
 public class EndState implements State {
 
-    private static final Logger logger = Logger.getLogger(EndState.class);
 
     @Override
     public void process(GameState gameState) {
         gameState.setState(this);
-        logger.info("State has changed to: " + gameState);
     }
 
 }
