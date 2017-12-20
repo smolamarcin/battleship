a88348df2f63892e8b2790dd511d3bee58274a04
rivalez <marektabor1@gmail.com>
2017-12-20 11:10:13 +0100

    fix scripts and readme

---
+++ runServer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mvn clean install;
+cd "battleship-server";
+echo Please, enter your IPv4 
+read ip
+java -cp target/battleship-server-1.0.1.jar com.epam.solid.nie.server.server.Main $ip
+
