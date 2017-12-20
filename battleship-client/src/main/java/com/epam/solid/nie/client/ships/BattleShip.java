8c4f52bfa26a4dca3630dea73de59fcd25e6a59b
rivalez <marektabor1@gmail.com>
2017-12-20 10:55:48 +0100

    fix to stable version

--- battleship-client/src/main/java/com/epam/solid/nie/client/ships/BattleShip.java
+++
@@ -1,8 +0,0 @@
-package com.epam.solid.nie.ships;
-
-/**
- * Created by marek on 13.12.2017.
- */
-public interface BattleShip extends BattleShipProperties, ShipHealth {
-    BattleShipType getBattleShipType();
-}
--- battleship-client/src/main/java/com/epam/solid/nie/client/state/State.java
+++
@@ -1,7 +0,0 @@
-package com.epam.solid.nie.state;
-
-public interface State {
-
-    void process(GameState gameState);
-
-}
