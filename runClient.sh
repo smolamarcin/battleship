32bda69c466fd7bff8df8d1a93c0bc534cf733bc
Dominik <saidamyzo@gmail.com>
2017-12-28 17:05:42 +0100

    Change commands to run server jar and client jar

--- runClient.sh
+++ runClient.sh
@@ -6,4 +6,4 @@ echo Running mvn package command
 mvn clean install;
 
 echo Starting application
-java -cp target/battleship-client-1.0.1.jar com.epam.solid.nie.client.ui.StartScene
+java -jar target/battleship-client-1.0.1.jar
--- runServer.sh
+++ runServer.sh
@@ -4,5 +4,5 @@ mvn clean install;
 cd "battleship-server";
 echo Please, enter your IPv4 
 read ip
-java -cp target/battleship-server-1.0.1.jar com.epam.solid.nie.server.server.Main $ip
+java -jar target/battleship-server-1.0.1.jar $ip
 
