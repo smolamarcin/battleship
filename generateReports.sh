c12f5855b67551b84ffe1ef112fe5c55a4db58d0
Marcin <marcin@MBP-Macbook.budapest.epam.com>
2018-01-03 18:15:09 +0100

    checkstyle script

---
+++ generateReports.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Script used to generate reports.
+#Inspired by the scripts by Mateusz Tapa.
+
+function usage(){
+	echo "Usage: $0 [-site | -jdepend | -checkstyle | -findbugs | -jacoco | -sonar | -all]"
+	exit 1
+}
+
+function checkstyle(){
+cd $(git rev-parse --show-toplevel)
+	if [[ ! $(find . -name 'checkstyle.html') ]]; then
+	mvn site checkstyle:checkstyle
+	fi
+	xargs -a reports/findbugs.txt firefox -new-tab "$line"
+}
+
+case $1 in
+#	"-site") site;;
+#	"-jdepend") jdepend;;
+	"-checkstyle") checkstyle;;
+#	"-findbugs") findbugs;;
+#	"-jacoco") jacoco;;
+#	"-sonar") sonar;;
+#	"-all") all;;
+*) usage
+esac
+
+
