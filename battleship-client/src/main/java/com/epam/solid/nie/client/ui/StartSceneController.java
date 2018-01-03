54299ac3480cf0b6adbf3c7e04fa59b6f1c52d7d
Marcin <marcin@MBP-Macbook.budapest.epam.com>
2018-01-03 10:19:43 +0100

    added image to starting screen

--- battleship-client/src/main/java/com/epam/solid/nie/client/ui/StartSceneController.java
+++ battleship-client/src/main/java/com/epam/solid/nie/client/ui/StartSceneController.java
@@ -8,11 +8,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 
 public class StartSceneController {
-
+    @FXML
+    private ImageView logo;
     @FXML
     private Button btn_connect;
 
