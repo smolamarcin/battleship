f99e58c36dfe333b60893176061ef6a81b07be31
Marcin <marcin@MBP-Macbook.budapest.epam.com>
2018-01-04 08:23:32 +0100

    reports txt files (paths to reports)

--- generateReports.sh
+++ generateReports.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 #Script used to generate reports.
 #Inspired by the scripts by Mateusz Tapa.
-#
+
 function usage(){
 	echo "Usage: $0 [-site | -jdepend | -checkstyle | -findbugs | -jacoco | -sonar | -all]"
 	exit 1
 }
 
+
 function checkstyle(){
-cd $(git rev-parse --show-toplevel)
-	if [[ ! $(find . -name 'checkstyle.html') ]]; then
-	mvn site checkstyle:checkstyle
-	fi
+cd $(git rev-parse --show-toplevel);
 	xargs -a reports/checkstyle.txt firefox -new-tab "$line"
 }
 
-case $1 in
-#	"-site") site;;
-#	"-jdepend") jdepend;;
-	"-checkstyle") checkstyle;;
-#	"-findbugs") findbugs;;
-#	"-jacoco") jacoco;;git s
-#	"-sonar") sonar;;
-#	"-all") all;;
-*) usage
-esac
+function findbugs(){
+cd $(git rev-parse --show-toplevel);
+    xargs -a reports/findbugs.txt firefox -new-tab "$line"
+}
+function sonar(){
+cd $(git rev-parse --show-toplevel);
+    xargs -a scripts/reports/sonar.txt firefox -new-tab "$line"
+}
+function jacoco(){
+mvn jacoco:report
+    cd $(git rev-parse --show-toplevel);
+    xargs -a scripts/reports/jacoco.txt firefox -new-tab "$line"
+}
+
+function all(){
+    mvn clean;
+    mvn install site;
+    findbugs;
+    checkstyle;
+    jacoco;
+
+}
 
 
