1208c9c8c0cfea2a5b2dd7dec868c04a80b5ed2a
Marcin <marcin@MBP-Macbook.budapest.epam.com>
2018-01-04 15:01:12 +0100

    added java docs to the server

--- battleship-server/src/main/java/com/epam/solid/nie/config/FileConfiguration.java
+++ battleship-server/src/main/java/com/epam/solid/nie/config/FileConfiguration.java
@@ -6,10 +6,18 @@ import java.util.ResourceBundle;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+/**
+ * Reads configuration from the file.
+ */
 public class FileConfiguration implements Configuration {
 
     private EnumMap<ConfigProperty, String> map = new EnumMap(ConfigProperty.class);
 
+    /**
+     * Return the config map which is used in the game.
+     * The map is filled based on the configuration file.
+     * @return
+     */
     public Map<ConfigProperty, String> provide() {
         if (map.isEmpty()) {
             ResourceBundle resourceBundle = ResourceBundle.getBundle(Configuration.CONFIGURATION);
