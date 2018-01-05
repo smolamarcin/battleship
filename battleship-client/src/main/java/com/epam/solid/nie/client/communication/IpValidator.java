4aa21e6eb5a848efd9c72a2071a7dd2621a9d6de
Marcin <marcin@MBP-Macbook.budapest.epam.com>
2018-01-05 11:37:12 +0100

    reduced checkstyle smells in IpValidator

--- battleship-client/src/main/java/com/epam/solid/nie/client/communication/IpValidator.java
+++ battleship-client/src/main/java/com/epam/solid/nie/client/communication/IpValidator.java
@@ -10,20 +10,25 @@ package com.epam.solid.nie.client.communication;
 
 public class IpValidator implements Validator {
     /**
-     * Determines whether the given IP is in the correct format and does not contain invalid characters.
+     * Determines whether the given IP is in the correct format
+     * and does not contain invalid characters.
      * Works for IPv4.
      *
      * @param ip
      * @return true if IP is in correct format
      */
+
     @Override
     public boolean validate(String ip) {
         String[] splitted = ip.split("\\.");
-        if (splitted.length != 4)
+        if (splitted.length != 4) {
             return false;
-        for (String s : splitted)
-            if (!(s.matches("[1-9]\\d*") && Integer.parseInt(s) < 256))
+        }
+        for (String s : splitted) {
+            if (!(s.matches("[1-9]\\d*") && Integer.parseInt(s) < 256)) {
                 return false;
+            }
+        }
         return true;
     }
 }
